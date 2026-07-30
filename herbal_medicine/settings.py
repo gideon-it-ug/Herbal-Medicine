9ea04c037ef1409f9a7a2250f03074421f5cf905
MUGABE GIDEON <mugabegideon44@gmail.com>
2026-07-30 19:15:16 +0300

    Fix: add STATIC_ROOT for collectstatic, fix DB_USER to match Render database credentials

--- herbal_medicine/settings.py
+++ herbal_medicine/settings.py
@@ -211,6 +211,8 @@ X_FRAME_OPTIONS = "DENY"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Cloudinary configuration
 CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME', '')
